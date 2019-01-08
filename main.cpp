9eed913a528cb020dd81f531b9a6d31d63f4f6aa
Ivan Pohodnya <Pohodnyaivan@gmail.com>
2019-01-08 18:08:44 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,3 @@
-//
-// Created by ivan on 08.04.16.
-//
-
 #include <iostream>
 #include <algorithm>
 #include <chrono>
@@ -59,7 +55,6 @@ void createRandomList(Test* nodesArray,int size){
         nodesArray[i].r = i;
     }
     set<int> integerSet;
-//    int randomSize = int(SAMPLES_NUMB);
     int random[size];
     for (int i = 0; i < size; ++i) {
         random[i] = i;
@@ -71,7 +66,6 @@ void createRandomList(Test* nodesArray,int size){
     while (i != size) {
         int randInt = random[k % size];
         k++;
-//        randInt = randInt % SAMPLES_NUMB;
         if (integerSet.find(randInt) == integerSet.end()) {
             if (nodesArray[randInt].r != start->r) {
                 start->test = &nodesArray[randInt];
@@ -92,26 +86,12 @@ void randomAccess(int step){
     ofstream ofs("random.csv");
     Test* t = new Test[SAMPLES_NUMB];
     createRandomList(t,SAMPLES_NUMB);
-//        Test * t = new Test[size];
 
     register int m,j;
     size_t size = step;
     int repeat = 5;
     while(size < SAMPLES_NUMB) {
-
         Test *tt;
-//        int repeat = 0;
-//        if(size > 100000){
-//            repeat = 5;
-//        }else{
-//            repeat = 5;
-//        }
-//        for(int i = 0;i < repeat; ++i) {
-//            tt = &t[0];
-//            for (m = 0; m < size; ++m) {
-//                tt = tt->test;
-//            }
-//        }
         timespec begin, end;
         clock_gettime(CLOCK_REALTIME,&begin);
         for(j = 0; j < repeat; ++j) {
@@ -141,167 +121,3 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
-
-
-
-/* each time create new list
-void randomAccess(int step){
-    register int m;
-    size_t size = step;
-    ofstream ofs("random.csv");
-    while(size < SAMPLES_NUMB) {
-
-        Test t[size];
-//        Test * t = new Test[size];
-        for (int i = 0; i < size; ++i) {
-            t[i].r = i;
-        }
-        set<int> integerSet;
-//        int randomSize = int(size * 1.5);
-        int random[size];
-        for (int i = 0; i < size; ++i) {
-            random[i] = i;
-        }
-        random_shuffle(random, random + size);
-        int i = 0;
-        int k = 0;
-        Test *start = &t[0];
-        while (i != size) {
-            int randInt = random[k % size];
-            k++;
-            randInt = randInt % size;
-            if (integerSet.find(randInt) == integerSet.end()) {
-                if (t[randInt].r != start->r) {
-                    start->test = &t[randInt];
-                    integerSet.insert(start->r);
-                    start = start->test;
-                    i++;
-                } else {
-                    if (i == size - 1) {
-                        start->test = NULL;
-                        break;
-                    }
-                }
-            }
-        }
-        auto begin = std::chrono::high_resolution_clock::now();
-        Test *tt = t[0].test;
-        for(m = 0;m < size;m+=step){
-            tt = tt->test;
-            tt = tt->test;
-            tt = tt->test;
-        }
-        auto end = chrono::high_resolution_clock::now();
-        ofs << size*sizeof(Test) << ',' << chrono::duration_cast<std::chrono::nanoseconds>(end-begin).count()/size << "\n";
-        if(size < L1CACHE/sizeof(Test)){
-            size += step;
-        }else{
-            size += step * 8;
-        }
-//        delete[] t;
-//        delete[] random;
-    }
-}
-
-
- void consequentAccess(int step){
-    register int m;
-    size_t size = step;
-    ofstream ofs("consequent.csv");
-    while(size < SAMPLES_NUMB){
-        Test* t = new Test();
-        Test* temp = t;
-        for(int i = 0;i < size; ++i){
-            t->test = new Test();
-            t = t->test;
-        }
-        t = temp;
-
-        auto begin = std::chrono::high_resolution_clock::now();
-        Test *tt = t->test;
-        for(m = 0;m < size;m+=step){
-            tt = tt->test;
-            tt = tt->test;
-            tt = tt->test;
-        }
-        auto end = chrono::high_resolution_clock::now();
-        ofs << size*sizeof(Test) << ',' << chrono::duration_cast<std::chrono::nanoseconds>(end-begin).count()/size << "\n";
-        if(size < L1CACHE/sizeof(Test)){
-            size += step;
-        }else{
-            size += step * 8;
-        }
-        while(t->test != NULL){
-            temp = t->test;
-            delete t;
-            t = temp;
-        }
-    }
-    ofs.close();
-}*/
-
-
-/* random heap list
-   void createRandomList(Test* nodesArray,int size){
-    for (int i = 0; i < size; ++i) {
-        nodesArray[i].r = i;
-    }
-    set<int> integerSet;
-//    int randomSize = int(SAMPLES_NUMB);
-    int random[size];
-    for (int i = 0; i < size; ++i) {
-        random[i] = i;
-    }
-    random_shuffle(random, random + size);
-    int i = 0;
-    int k = 0;
-    Test *start = &nodesArray[0];
-    while (i != size) {
-        int randInt = random[k % size];// k can be > samples
-        k++;
-//        randInt = randInt % SAMPLES_NUMB;
-        if (integerSet.find(randInt) == integerSet.end()) {
-            if (nodesArray[randInt].r != start->r) {
-                start->test = &nodesArray[randInt];
-                integerSet.insert(start->r);
-                start = start->test;
-                i++;
-            } else {
-                if (i == size - 1) {
-                    start->test = NULL;
-                    break;
-                }
-            }
-        }
-    }
-}
-void randomAccess(int step){
-
-    ofstream ofs("random.csv");
-    Test t[SAMPLES_NUMB];
-    createRandomList(t,SAMPLES_NUMB);
-//        Test * t = new Test[size];
-
-    register int m;
-    size_t size = step;
-    while(size < SAMPLES_NUMB) {
-
-
-        auto begin = std::chrono::high_resolution_clock::now();
-        Test *tt = t[0].test;
-        for(m = 0;m < size;m+=step){
-            tt = tt->test;
-            tt = tt->test;
-            tt = tt->test;
-        }
-        auto end = chrono::high_resolution_clock::now();
-        ofs << size*sizeof(Test) << ',' << chrono::duration_cast<std::chrono::nanoseconds>(end-begin).count()/size << "\n";
-        if(size < L1CACHE/sizeof(Test)){
-            size += step;
-        }else{
-            size += step * 4;
-        }
-//        delete[] t;
-//        delete[] random;
-    }
-}*/
